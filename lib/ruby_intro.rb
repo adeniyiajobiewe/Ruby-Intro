c8b923e830dd382cd73cf16787d61c431f1aa38c
EC2 Default User <ec2-user@ip-172-31-15-231.us-east-2.compute.internal>
2022-09-12 01:58:02 +0000

    Completed assignment in ruby_intro file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,131 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  
+  result = 0
+  if arr.length > 0 then
+    arr.each do |item|
+      result += item
+    end
+  else
+    result = 0
+  end
+  return result
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+
+
+def max_2_sum(arr)
+    
+  if arr.length > 1
+    largestNumber = arr.max()
+    arr[arr.index(largestNumber)] = arr.min()
+    secondLargestNumber = arr.max()
+    sum = largestNumber + secondLargestNumber
+  elsif arr.length == 1
+    sum = arr[0]
+  else
+    sum = 0
+  end
+  return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+
+def sum_to_n?(array, n)
+    hashMap = {}
+    array.each_with_index do |integer, index|
+        if hashMap.has_key?(n - integer) then
+            return true
+        else
+            hashMap[integer] = index
+        end
+    
+    end
+    return false
 end
+
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+
+  return ("Hello, " + name)
+  
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    consonants = ('A'..'Z').to_a - vowels
+    if consonants.include?(s.chr.upcase) then
+        return true
+    else
+        return false
+    end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+
+    count_arr = Array.new
+    
+    letters = ('a'..'z').to_a
+    nonbinary = ('!'..'?').to_a - ['0', '1'] + [' ']
+    if s.length == 0
+        return false
+    end
+    for i in s.split("")
+      
+        if nonbinary.include?(i) or letters.include?(i.downcase) then
+            count_arr << i
+        end
+    end
+    if count_arr.length == 0 and s.to_i % 4 == 0 then
+        return true
+    else
+        return false
+    end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @isbn=isbn
+  end
+  
+  def price=(price)
+    @price=price
+  end
+  
+  
+  def initialize(isbn, price)
+    if isbn.length >= 1
+      @isbn = isbn
+    else
+      raise ArgumentError, 'ISBN number should not be an empty string'
+    end
+    if price > 0
+      @price = price
+    else
+      raise ArgumentError, "Price can not be 0 or negative"
+    end
+  end
+  
+  def price_as_string
+    return "$" + ('%.2f' % @price).to_s
+    
+  end
+  
+  
 end
